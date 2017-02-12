dfefcf9db03ee1323c65f2b6223ccc85b0126ef3
vascofornas <desarrollo@webjuarez.com>
2017-02-12 02:43:10 -0700

    TERMINADAS CITAS

--- scripts/enviarCita.php
+++ scripts/enviarCita.php
@@ -8,13 +8,16 @@ $modelo = $_POST['d'];
 $ano = $_POST['e'];
 
 $km = $_POST['f'];
-$fecha = htmlentities($_REQUEST["fecha"]);
-$hora = htmlentities($_REQUEST["hora"]);
+$tipo = $_POST['g'];
+$fecha = $_POST['h'];
+$hora = $_POST['i'];
+$comentarios = $_POST['j'];
+$codigo = $_POST['k'];
+$agencia = $_POST['l'];
+$email_taller = $_POST['m'];
+$email_asesor = $_POST['n'];
 
-$tipo = htmlentities($_REQUEST["tipo"]);
-$taller = htmlentities($_REQUEST["taller"]);
-$comentarios = htmlentities($_REQUEST["comentarios"]);
-$emailAsesor = htmlentities($_REQUEST["emailAsesor"]);
+
 
 
     $host='localhost';
@@ -32,13 +35,134 @@ mysqli_query($link,"INSERT INTO tb_citas_servicio (`nombre_cliente`,`email_clien
 `agencia_cita`)
 VALUES ('$nombre_usuario','$email_usuario','$tel_usuario',
 '$modelo','$ano',
-'$km ,'Tip izdelka (6)',
-'Tip izdelka (6)','Tip izdelka (6)',
-'Tip izdelka (6)','Tip izdelka (6)',
-'Tip izdelka (6)')") 
+'$km' ,'$tipo',
+'$fecha','$hora',
+'$comentarios','$codigo',
+'$agencia')") 
 or die(mysqli_error($link));
    
-echo 'Successfully addedlo.'.$km;
+
+
+$to = $email_taller;
+$subject = '(EMAIL AL TALLER) Cita a Servicio '.$codigo.' / '.$comentarios.' desde la app Mi Asesor Automotriz (iOS)';
+$from = 'Mi Asesor Automotriz';
+ 
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Create email headers
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+ 
+// Compose a simple HTML email message
+$message = '<html><body>';
+$message .= '<h1 style="color:#f40;">Hola '.$nombre_usuario.'!</h1>';
+$message .= '<p style="color:#080;font-size:18px;">Esto es una CITA A SERVICIO creada desde la App MI ASESOR AUTOMOTRIZ</p>';
+$message .= '<p>Cliente: '.$nombre_usuario.'</p>';
+$message .= '<p>Email del Cliente: '.$email_usuario.'</p>';
+$message .= '<p>Teléfono del Cliente: '.$tel_usuario.'</p>';
+$message .= '<p>Auto: '.$modelo.'</p>';
+$message .= '<p>Año: '.$ano.'</p>';
+$message .= '<p>Kilómetros: '.$km.'</p>';
+$message .= '<p>Tipo de cita: '.$tipo.'</p>';
+$message .= '<p>Fecha de la cita: '.$fecha.'</p>';
+$message .= '<p>Hora de la cita: '.$hora.'</p>';
+$message .= '<p>Comentarios del cliente: '.$comentarios.'</p>';
+$message .= '<p>Código de la cita: '.$codigo.'</p>';
+$message .= '<p>Gracias por su confianza. En breve nos pondremos en contacto con usted</p>';
+
+
+$message .= '</body></html>';
+ 
+// Sending email
+if(mail($to, $subject, $message, $headers)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
+
+$to = $email_usuario;
+$subject = '(EMAIL AL CLIENTE) Cita a Servicio '.$codigo.' / '.$comentarios.' desde la app Mi Asesor Automotriz (iOS)';
+$from = 'Mi Asesor Automotriz';
+ 
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Create email headers
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+ 
+// Compose a simple HTML email message
+$message = '<html><body>';
+$message .= '<h1 style="color:#f40;">Hola '.$nombre_usuario.'!</h1>';
+$message .= '<p style="color:#080;font-size:18px;">Esto es una CITA A SERVICIO creada desde la App MI ASESOR AUTOMOTRIZ</p>';
+$message .= '<p>Cliente: '.$nombre_usuario.'</p>';
+$message .= '<p>Email del Cliente: '.$email_usuario.'</p>';
+$message .= '<p>Teléfono del Cliente: '.$tel_usuario.'</p>';
+$message .= '<p>Auto: '.$modelo.'</p>';
+$message .= '<p>Año: '.$ano.'</p>';
+$message .= '<p>Kilómetros: '.$km.'</p>';
+$message .= '<p>Tipo de cita: '.$tipo.'</p>';
+$message .= '<p>Fecha de la cita: '.$fecha.'</p>';
+$message .= '<p>Hora de la cita: '.$hora.'</p>';
+$message .= '<p>Comentarios del cliente: '.$comentarios.'</p>';
+$message .= '<p>Código de la cita: '.$codigo.'</p>';
+$message .= '<p>Gracias por su confianza. En breve nos pondremos en contacto con usted</p>';
+
+
+$message .= '</body></html>';
+ 
+// Sending email
+if(mail($to, $subject, $message, $headers)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
+
+$to = $email_asesor;
+$subject = '(EMAIL AL ASESOR) Cita a Servicio '.$codigo.' / '.$comentarios.' desde la app Mi Asesor Automotriz (iOS)';
+$from = 'Mi Asesor Automotriz';
+ 
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Create email headers
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+ 
+// Compose a simple HTML email message
+$message = '<html><body>';
+$message .= '<h1 style="color:#f40;">Hola '.$nombre_usuario.'!</h1>';
+$message .= '<p style="color:#080;font-size:18px;">Esto es una CITA A SERVICIO creada desde la App MI ASESOR AUTOMOTRIZ</p>';
+$message .= '<p>Cliente: '.$nombre_usuario.'</p>';
+$message .= '<p>Email del Cliente: '.$email_usuario.'</p>';
+$message .= '<p>Teléfono del Cliente: '.$tel_usuario.'</p>';
+$message .= '<p>Auto: '.$modelo.'</p>';
+$message .= '<p>Año: '.$ano.'</p>';
+$message .= '<p>Kilómetros: '.$km.'</p>';
+$message .= '<p>Tipo de cita: '.$tipo.'</p>';
+$message .= '<p>Fecha de la cita: '.$fecha.'</p>';
+$message .= '<p>Hora de la cita: '.$hora.'</p>';
+$message .= '<p>Comentarios del cliente: '.$comentarios.'</p>';
+$message .= '<p>Código de la cita: '.$codigo.'</p>';
+$message .= '<p>Gracias por su confianza. En breve nos pondremos en contacto con usted</p>';
+
+
+$message .= '</body></html>';
+ 
+// Sending email
+if(mail($to, $subject, $message, $headers)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
+echo 'Successfully addedlo.'.$tipo;
 
 
 ?>
